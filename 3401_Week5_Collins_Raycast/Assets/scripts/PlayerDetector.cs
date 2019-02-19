864374cf26bdc0b22e9bc4e1c0de230c41a9e9dd
Sean Collins <sc679115@ohio.edu>
2019-02-19 11:15:57 -0500

    HW 1

---
+++ 3401_Week5_Collins_Raycast/Assets/scripts/PlayerDetector.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDetector : MonoBehaviour
+
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+      RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 5f)) 
+
+        {
+
+            Debug.Log(hit.collider);
+
+        } 
+        
+        else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+            Debug.Log("NOTHING AHEAD");
+        }
+    }
+}
---
+++ 3401_Week5_Collins_Raycast/Assets/scripts/PlayerMotor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMotor : MonoBehaviour
+{
+
+    public float PlayerMovement;
+    public NavMeshAgent agent;
+    public Transform destination;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        agent.SetDestination (destination.position);
+        
+    }
+}
